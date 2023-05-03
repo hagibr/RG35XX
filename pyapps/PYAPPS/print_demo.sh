f9a80b241bc62a4620880cd9d18282982974da1c
hagibr <alexandre.hagihara@gmail.com>
2023-05-03 17:43:22 -0300

    Rolling back the fbset command

--- pyapps/PYAPPS/print_demo.sh
+++ pyapps/PYAPPS/print_demo.sh
@@ -2,10 +2,9 @@
 SCRIPT_DIR=$(busybox dirname "$0")
 PYTHONPATH=${SCRIPT_DIR}/Python
 HOME=${SCRIPT_DIR}/framebuffer
-PROG_PATH=${HOME}/test_fb.py
+PROG_PATH=${HOME}/logging.py
 export LD_LIBRARY_PATH="${PYTHONPATH}/lib"
 cd ${PYTHONPATH}
 busybox fbset -depth 16
 ./python3.10 ${PROG_PATH}
 sync
-
